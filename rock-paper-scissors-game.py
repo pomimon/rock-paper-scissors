19c952bb661c4182f750fa6ff9717d2895a869b9
pomimon <bright.dusk1649@fastmail.com>
2025-01-31 11:41:10 +0100

    Add game logic

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -21,6 +21,24 @@ def main():
     comp_choice = random.choice(options)
     print(f"you chose {user_choice}, computer chose {comp_choice}")
 
+    if user_choice == comp_choice:
+        print("it's a tie")
+    elif user_choice == "rock" and comp_choice == "paper":
+        print("you lose, paper cover's rock")
+    elif user_choice == "paper" and comp_choice == "rock":
+        print("you won! paper cover's rock")
+
+    elif user_choice == "scissors" and comp_choice == "paper":
+        print("you won! scissors cut's paper")
+    elif user_choice == "paper" and comp_choice == "scissors":
+        print("you lose, scissors cut's paper")
+
+    elif user_choice == "rock" and comp_choice == "scissors":
+        print("you won! rock smashes scissors")
+    elif user_choice == "scissors" and comp_choice == "rock":
+        print("you lose! rock smashes scissors")
+
+
 
 
 
