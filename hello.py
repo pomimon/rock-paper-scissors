e03a81806e7c75d75e33484baad81e65f2653f4a
pomimon <bright.dusk1649@fastmail.com>
2025-01-31 10:40:56 +0100

    Add user input

--- hello.py
+++ hello.py
@@ -1,5 +1,28 @@
+import random
+
+
 def main():
-    print("Hello from rock-paper-scissors-app!")
+
+
+# random.choice to select a random string in options, list options
+# comp_input and user_input
+# if elif else statements
+#   - tie game (user_input == computer input)
+#   - rock beats scissors, paper covers rock,
+#   - scissors cuts paper
+
+# to-do later
+# while loop to keep playing
+
+    user_input = input("Enter your choice: ")
+    options = ['rock', 'paper', 'scissors']
+    comp_input = random.choice(options)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
