9f3a80438f6f09a5c74643aab5e03019f92df250
pomimon <bright.dusk1649@fastmail.com>
2025-01-31 22:43:36 +0100

    Add game loop

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -1,26 +1,16 @@
 import random
 
 
-def main():
+def read_choice(choices):
+    choice = None
+
+    while choice not in choices:
+        choice = input("Pick one of {choices}: ".format(choices=", ".join(choices)))
+
+    return choice
 
 
-# random.choice to select a random string in options, list options
-# comp_input and user_input
-# if elif else statements
-#   - tie game (user_input == computer input)
-#   - rock beats scissors, paper covers rock,
-#   - scissors cuts paper
-
-# to-do later
-# while loop to keep playing
-
-    print("Let's play rock, paper, scissors")
-
-    user_choice = input("Enter your choice: ")
-    options = ["rock", "paper", "scissors"]
-    comp_choice = random.choice(options)
-    print(f"you chose {user_choice}, computer chose {comp_choice}")
-
+def determine_winner(user_choice, comp_choice):
     if user_choice == comp_choice:
         print("it's a tie")
     elif user_choice == "rock" and comp_choice == "paper":
@@ -39,15 +29,29 @@ def main():
         print("you lose! rock smashes scissors")
 
 
+def main():
+    # random.choice to select a random string in options, list options
+    # comp_input and user_input
+    # if elif else statements
+    #   - tie game (user_input == computer input)
+    #   - rock beats scissors, paper covers rock,
+    #   - scissors cuts paper
+    # while loop to keep playing
+    # user picks wrong choice option
+    while True:
+        print("Let's play rock, paper, scissors")
 
+        # user_choice = input("Enter your choice: ")
+        options = ["rock", "paper", "scissors"]
+        user_choice = read_choice(options)
+        comp_choice = random.choice(options)
+        print(f"you chose {user_choice}, computer chose {comp_choice}")
 
+        determine_winner(user_choice, comp_choice)
 
-
-
-
-
-
-
+        play_again = input("Would you like to play again? (y/n): ")
+        if play_again.lower() != "y":
+            break
 
 
 if __name__ == "__main__":
